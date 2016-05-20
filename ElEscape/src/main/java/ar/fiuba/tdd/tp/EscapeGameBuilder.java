2e781aeb304343d6d4e349a958a2e6bda579bf71
Nicolas Cian <nicolas.cian@gmail.com>
2016-05-19 23:11:48 -0300

    Primer intento

---
+++ ElEscape/src/main/java/ar/fiuba/tdd/tp/EscapeGameBuilder.java
@@ -0,0 +1,369 @@
+package ar.fiuba.tdd.tp;
+
+import ar.fiuba.tdd.tp.action.ComplexAction;
+import ar.fiuba.tdd.tp.action.SimpleAction;
+import ar.fiuba.tdd.tp.action.simple.MessageSimpleAction;
+import ar.fiuba.tdd.tp.action.simple.MoveChildSimpleAction;
+import ar.fiuba.tdd.tp.action.simple.parameter.ExplicitParameter;
+import ar.fiuba.tdd.tp.action.simple.parameter.ImplicitParameter;
+import ar.fiuba.tdd.tp.action.simple.parameter.Parameter;
+import ar.fiuba.tdd.tp.condition.ChildrenSizeEqualsCondition;
+import ar.fiuba.tdd.tp.condition.FalseCondition;
+import ar.fiuba.tdd.tp.condition.HasChildCondition;
+import ar.fiuba.tdd.tp.condition.TrueCondition;
+import ar.fiuba.tdd.tp.condition.core.Condition;
+import ar.fiuba.tdd.tp.model.Game;
+import ar.fiuba.tdd.tp.model.GameBuilder;
+import ar.fiuba.tdd.tp.model.GameObject;
+
+public class EscapeGameBuilder implements GameBuilder {
+
+    @Override
+    public Game build() {
+        GameObject scene = buildScene();
+        Game game = new Game(scene);
+        addActions(game);
+
+        return game;
+    }
+
+    private void addActions(Game game) {
+        //game.addAction(buildGotoBibliotecaComplexAction(game));
+        game.addAction(buildGotoComplexAction(game));
+        game.addAction(buildPickComplexAction(game));
+        game.addAction(buildMoveCuadroBarcoComplexAction(game));
+        game.addAction(buildOpenComplexAction(game));
+        game.addAction(buildPutComplexAction(game));
+        game.addAction(buildShowComplexAction(game));
+    }
+
+    private GameObject buildScene() {
+        GameObject scene = new GameObject("scene");
+        GameObject pasillo = new GameObject("Pasillo");
+        scene.addChild(buildRoom1());
+        scene.addChild(buildRoom2(pasillo));
+        scene.addChild(buildRoom3(pasillo));
+        scene.addChild(buildRoom4(pasillo));
+        scene.addChild(buildRoom5(pasillo));
+        scene.addChild(buildRoom6());
+        scene.addChild(buildRoom7());
+        scene.addChild(buildRoom8());
+
+        return scene;
+    }
+
+    private GameObject buildRoom1() {
+        GameObject pasillo = new GameObject("Pasillo");
+        GameObject foto = new GameObject("Foto");
+        // TODO: Add lapicera
+        //GameObject lapicera = new GameObject("Lapicera");
+        GameObject player = new GameObject("player");
+        player.addChild(foto);
+        //player.addChild(lapicera);
+
+        pasillo.addChild(player);
+
+        return pasillo;
+    }
+
+    private GameObject buildRoom2(GameObject pasillo) {
+        GameObject salon1 = new GameObject("Salon1");
+        GameObject mesa = new GameObject("Mesa");
+        GameObject botella = new GameObject("Botella");
+        GameObject vaso1 = new GameObject("Vaso1");
+        GameObject vaso2 = new GameObject("Vaso2");
+        mesa.addChild(botella);
+        mesa.addChild(vaso1);
+        mesa.addChild(vaso2);
+
+        GameObject silla1 = new GameObject("Silla1");
+        GameObject silla2 = new GameObject("Silla2");
+        GameObject cuadroTren = new GameObject("CuadroTren");
+        GameObject cuadroBarco = new GameObject("CuadroBarco");
+        GameObject cajaFuerte = new GameObject("CajaFuerte");
+        cuadroBarco.addChild(cajaFuerte);
+        GameObject credencial = new GameObject("Credencial");
+        cajaFuerte.addChild(credencial);
+
+        salon1.addChild(mesa);
+        salon1.addChild(silla1);
+        salon1.addChild(silla2);
+        salon1.addChild(cuadroTren);
+        salon1.addChild(cuadroBarco);
+
+        return salon1;
+    }
+
+    private GameObject buildRoom3(GameObject pasillo) {
+        GameObject salon2 = new GameObject("Salon2");
+        GameObject martillo = new GameObject("Martillo");
+        GameObject destornillador1 = new GameObject("Destornillador1");
+        GameObject destornillador2 = new GameObject("Destornillador2");
+        salon2.addChild(martillo);
+        salon2.addChild(destornillador1);
+        salon2.addChild(destornillador2);
+
+        return salon2;
+    }
+
+    private GameObject buildRoom4(GameObject pasillo) {
+        GameObject salon3 = new GameObject("Salon3");
+        GameObject llave = new GameObject("Llave");
+        salon3.addChild(llave);
+
+        return salon3;
+    }
+
+    private GameObject buildRoom5(GameObject pasillo) {
+        GameObject bibliotecaAcceso = new GameObject("BibliotecaAcceso");
+        GameObject bibliotecario = new GameObject("Bibliotecario");
+        bibliotecaAcceso.addChild(bibliotecario);
+        bibliotecaAcceso.addChild(pasillo);
+
+        return bibliotecaAcceso;
+    }
+
+    private GameObject buildRoom6() {
+        GameObject biblioteca = new GameObject("Biblioteca");
+        GameObject estante = new GameObject("Estante");
+        GameObject libroViejo = new GameObject("LibroViejo");
+        GameObject libro1 = new GameObject("Libro1");
+        GameObject libro2 = new GameObject("Libro2");
+        GameObject libro3 = new GameObject("Libro3");
+        GameObject libro4 = new GameObject("Libro4");
+        GameObject libro5 = new GameObject("Libro5");
+        GameObject libro6 = new GameObject("Libro6");
+        GameObject libro7 = new GameObject("Libro7");
+        GameObject libro8 = new GameObject("Libro8");
+        GameObject libro9 = new GameObject("Libro9");
+
+        biblioteca.addChild(estante);
+        biblioteca.addChild(libro1);
+        biblioteca.addChild(libro2);
+        biblioteca.addChild(libro3);
+        biblioteca.addChild(libro4);
+        biblioteca.addChild(libro5);
+        biblioteca.addChild(libro6);
+        biblioteca.addChild(libro7);
+        biblioteca.addChild(libro8);
+        biblioteca.addChild(libro9);
+        biblioteca.addChild(libroViejo);
+
+        return biblioteca;
+    }
+
+    private GameObject buildRoom7() {
+        GameObject sotano = new GameObject("Sotano");
+        GameObject escalera = new GameObject("Escalera");
+        //GameObject baranda = new GameObject("Baranda");
+        sotano.addChild(escalera);
+        //sotano.addChild(baranda);
+
+        return sotano;
+    }
+
+    private GameObject buildRoom8() {
+        GameObject baranda = new GameObject("Baranda");
+        GameObject ventana = new GameObject("Ventana");
+        baranda.addChild(ventana);
+
+        return baranda;
+    }
+
+    private ComplexAction buildGotoComplexAction(Game game) {
+        String name = "goto";
+        String command = "goto <parameter>";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildGotoCantBibliotecaSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+        simpleAction = buildGotoBibliotecaSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+        simpleAction = buildGotoSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildGotoCantBibliotecaSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter bibliotecario = new ImplicitParameter("Bibliotecario");
+        Parameter credencial = new ImplicitParameter("Credencial");
+        Condition<String> condition = new HasChildCondition(game, bibliotecario, credencial).not(null);
+
+        String result = "No puede entrar";
+        return new MessageSimpleAction(complexAction, condition, result);
+    }
+
+    private SimpleAction buildGotoBibliotecaSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ImplicitParameter("player");
+        Parameter targetParameter = new ImplicitParameter("Biblioteca");
+        Parameter bibliotecario = new ImplicitParameter("Bibliotecario");
+        Parameter credencial = new ImplicitParameter("Credencial");
+        Condition<String> condition = new HasChildCondition(game, bibliotecario, credencial);
+
+        String result = "Entro";
+        return new MoveChildSimpleAction(complexAction, condition, childParameter,
+                targetParameter, result);
+    }
+
+    private SimpleAction buildGotoSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ImplicitParameter("player");
+        Parameter targetParameter = new ExplicitParameter(1);
+
+        String result = "Entro";
+        return new MoveChildSimpleAction(complexAction, new TrueCondition(), childParameter,
+                targetParameter, result);
+    }
+
+    /*private ComplexAction buildGotoBibliotecaComplexAction(Game game) {
+        String name = "goto Biblioteca";
+        String command = "goto Biblioteca";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildCantGotoBibliotecaSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+        simpleAction = buildGotoBibliotecaSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildGotoBibliotecaSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ImplicitParameter("player");
+        Parameter targetParameter = new ImplicitParameter("Biblioteca");
+        Parameter bibliotecario = new ImplicitParameter("Bibliotecario");
+        Parameter credencial = new ImplicitParameter("Credencial");
+        Condition<String> condition = new HasChildCondition(game, bibliotecario, credencial);
+
+        String result = "EntroBiblioteca";
+        return new MoveChildSimpleAction(complexAction, new TrueCondition(), childParameter,
+                targetParameter, result);
+    }
+
+    private SimpleAction buildCantGotoBibliotecaSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter bibliotecario = new ImplicitParameter("Bibliotecario");
+        Parameter credencial = new ImplicitParameter("Credencial");
+        Condition<String> condition = new HasChildCondition(game, bibliotecario, credencial).not(null);
+
+        String result = "No puede entrar";
+        return new MessageSimpleAction(complexAction, new FalseCondition(), result);
+    }*/
+
+    private ComplexAction buildPickComplexAction(Game game) {
+        String name = "pick";
+        String command = "pick <parameter>";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildCantPickSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+        simpleAction = buildPickSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildPickSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ExplicitParameter(1);
+        Parameter targetParameter = new ImplicitParameter("player");
+        Condition<String> condition = new ChildrenSizeEqualsCondition(game, targetParameter, 0).or(new ChildrenSizeEqualsCondition(game, targetParameter, 1)).or(new ChildrenSizeEqualsCondition(game, targetParameter, 2)).or(new ChildrenSizeEqualsCondition(game, targetParameter, 3));
+
+        String result = "Agarro";
+        return new MoveChildSimpleAction(complexAction, condition, childParameter,
+                targetParameter, result);
+    }
+
+    private SimpleAction buildCantPickSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter targetParameter = new ImplicitParameter("player");
+        Condition<String> condition = new ChildrenSizeEqualsCondition(game, targetParameter, 0).or(new ChildrenSizeEqualsCondition(game, targetParameter, 1)).or(new ChildrenSizeEqualsCondition(game, targetParameter, 2)).or(new ChildrenSizeEqualsCondition(game, targetParameter, 3)).not(null);
+        String result = "Ya tenes muchas cosas";
+
+        return new MessageSimpleAction(complexAction, condition, result);
+    }
+
+    private ComplexAction buildMoveCuadroBarcoComplexAction(Game game) {
+        String name = "move CuadroBarco";
+        String command = "move CuadroBarco";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildMoveCuadroBarcoSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildMoveCuadroBarcoSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter keyParameter = new ImplicitParameter("CajaFuerte");
+        Parameter room1Parameter = new ImplicitParameter("Salon1");
+
+        String result = "Movio";
+        return new MoveChildSimpleAction(complexAction, new TrueCondition(), keyParameter, room1Parameter,
+                result);
+    }
+
+    private ComplexAction buildOpenComplexAction(Game game) {
+        String name = "open CajaFuerte using";
+        String command = "open CajaFuerte using <parameter>";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildOpenLockedDoorSimpleAction(game, complexAction);
+        complexAction.getSteps().add(simpleAction);
+        simpleAction = buildOpenUnlockedDoorSimpleAction(game, complexAction);
+        complexAction.getSteps().add(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildOpenUnlockedDoorSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ImplicitParameter("Credencial");
+        Parameter targetParameter = new ImplicitParameter("Salon1");
+        Parameter player = new ImplicitParameter("player");
+        Parameter llave = new ExplicitParameter(3);
+        Condition<String> condition = new HasChildCondition(game, player, llave);
+
+        String result = "Abrio";
+        return new MoveChildSimpleAction(complexAction, condition, childParameter,
+                targetParameter, result);
+    }
+
+    private SimpleAction buildOpenLockedDoorSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter player = new ImplicitParameter("player");
+        Parameter llave = new ExplicitParameter(3);
+        Condition<String> condition = new HasChildCondition(game, player, llave).not(null);
+
+        String result = "Necesitas la Llave";
+        return new MessageSimpleAction(complexAction, condition, result);
+    }
+
+    private ComplexAction buildPutComplexAction(Game game) {
+        String name = "put";
+        String command = "put <parameter> in <parameter>";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildPutSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildPutSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ExplicitParameter(1);
+        Parameter targetParameter = new ExplicitParameter(3);
+
+        String result = "Puso";
+        return new MoveChildSimpleAction(complexAction, new TrueCondition(), childParameter,
+                targetParameter, result);
+    }
+
+    private ComplexAction buildShowComplexAction(Game game) {
+        String name = "show";
+        String command = "show <parameter> in <parameter>";
+        ComplexAction complexAction = new ComplexAction(name, command, game);
+        SimpleAction simpleAction = buildShowSimpleAction(game, complexAction);
+        complexAction.addAction(simpleAction);
+
+        return complexAction;
+    }
+
+    private SimpleAction buildShowSimpleAction(Game game, ComplexAction complexAction) {
+        Parameter childParameter = new ExplicitParameter(1);
+        Parameter targetParameter = new ExplicitParameter(3);
+
+        String result = "Mostro";
+        return new MoveChildSimpleAction(complexAction, new TrueCondition(), childParameter,
+                targetParameter, result);
+    }
+}
