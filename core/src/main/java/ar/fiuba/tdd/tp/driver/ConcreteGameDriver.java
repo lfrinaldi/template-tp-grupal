a993a5b56acdbb926cb3aad596ce469fe1482e44
Nicolas Cian <nicolas.cian@gmail.com>
2016-05-20 23:11:13 -0300

    Agrego el driver

---
+++ core/src/main/java/ar/fiuba/tdd/tp/driver/ConcreteGameDriver.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tdd.tp.driver;
+
+import ar.fiuba.tdd.tp.loader.GameLoader;
+import ar.fiuba.tdd.tp.model.Game;
+
+public class ConcreteGameDriver implements GameDriver {
+    protected Game game;
+
+    public void initGame(String jarPath) throws GameLoadFailedException {
+        GameLoader loader = new GameLoader();
+        game = loader.get("escape");
+    }
+
+    public String sendCommand(String cmd) {
+        return game.doCommand(cmd);
+    }
+
+    public GameState getCurrentState() {
+        String status = game.getScene().getAttribute("status");
+        switch (status) {
+            case "win":
+                return GameState.Won;
+            case "lost":
+                return GameState.Lost;
+            default:
+                return GameState.InProgress;
+        }
+    }
+}
